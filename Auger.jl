6ad3571a05665a8d73e47f2dffee76f260d893b2
Abdalaziz Rashid <abdalaziz.rashid@outlook.com>
2018-10-16 16:24:24 +0300

    Adding function

--- Auger.jl
+++ Auger.jl
@@ -2,112 +2,85 @@ using PyCall
 pygui(:qt5)
 using PyPlot
 using Gtk
+# Importing libraries
+let # Global scope
 
-let
+global num_y = 100000
+global num_x = 10
+global j = readlines("test.csv")
+global i = readlines("y.csv")
+global j_f = parse.([Float64], j)
+global i_f = parse.([Float64], i)
+global key1 = 1
+global data = i_f    ################################################## FIRST SET OF DATA INPUT
+global data_2 = j_f  ################################################## SECOND SET OF DATA INPUT
+global x = collect(1:length(data))
 
-  global num_y = 100000
-  global num_x = 10
+plot(data_2, color="brown", linewidth=2.0, linestyle="--")
+        title("AUGER")
+        xlabel("X")
+        ylabel("Y")
+        grid("on")
+global win = GtkWindow("Key Press Example") # to insurce default selection of the window
+# key listner
+signal_connect(win, "key-press-event") do widget, event
+println("You pressed key ", event.keyval)
+key1 = event.keyval
+println("hhhhh=$key1")
 
-  global j = readlines("test.csv")
-  global i = readlines("y.csv")
-  global j_f = parse.([Float64], j)
-  global i_f = parse.([Float64], i)
-
-  #global x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
-  #j_f = Vector[[12,212,23,24,34,4,3],[12,34,54,3,43,32,6,4]]
-  #i_f = Vector[[12,34,54,3,43,32,6,4],[12,34,54,3,43,32,6,4]]
-  #j_d = [1:40, j_f]
-  global key1 = 1
-  data = j_f    ################################################## DATA INPUT
-  global x = collect(1:length(data))
-  plot(data, color="red", linewidth=2.0, linestyle="--")
-              title("AUGER")
-              xlabel("X")
-              ylabel("Y")
-              grid("on")
-  global win = GtkWindow("Key Press Example") # to insurce default selection of the window
-  # key listner
-  signal_connect(win, "key-press-event") do widget, event
-  println("You pressed key ", event.keyval)
-  key1 = event.keyval
-  println("hhhhh=$key1")
-
-  # end of key listner
-  #PyPlot.pause(0.05)
+# end of key listner
+#PyPlot.pause(0.05)
 ################################ CONTROL
-if (key1 == 119)# W buttom
-PyPlot.cla()
+function Axis_shift_x(x, x_inc)
+        x = x .+ x_inc
+        return(x)
+end
+
+function Axis_shift_y(data, num_y)
+        y = y .+ y_inc
+        return(y)
+end
+
+
+function Plot_me(x, y, x_inc, y_inc)
+        PyPlot.cla()
+
+        plot(x, y, color="green", linewidth=2.0)
+                    title("AUGER")
+                    xlabel("X")
+                    ylabel("Y")
+                    grid("on")
+        plot(x_inc, y_inc, color="red", linewidth=2.0, linestyle="--")
+                title("AUGER")
+                xlabel("X")
+                ylabel("Y")
+                grid("on")
+        println("DAA=$key1")
+end
+
+if (key1 == 119)############################# W buttom
+# SECOND SET OF DATA
+num_y = num_y + 100000
+Plot_me(x, data, x, (data .+ num_y))
+
+
+println("in function")
 
 
 
-      num_y = num_y + 50000
-      data2 = j_f .+ num_y # SECOND SET OF DATA
 
-      plot(x, data.+ num_y, color="red", linewidth=2.0, linestyle="--")
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      plot(data, color="green", linewidth=2.0)
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      println("DAA=$key1")
 
 elseif (key1 == 115)# S buttom
-  PyPlot.cla()
+        num_x = num_x + 10
+        Plot_me(x, data, x, (data .- num_y))
 
+elseif (key1 == 97)################### A buttom
+        num_x = num_x + 10
+        Plot_me(x, data, (x .+ num_x), data)
 
-      num_y = num_y - 50000
-      data2 = j_f .+ num_y # SECOND SET OF DATA
-      plot(x, data.+ num_y, color="red", linewidth=2.0, linestyle="--")
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      plot(data, color="green", linewidth=2.0)
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      println("DAA=$key1")
-elseif (key1 == 97)# A buttom
-  PyPlot.cla()
-      println("DAA=$key1")
-
-      num_x = num_x - 10
-      xc = x .+ num_x
-      data2 = j_f  # SECOND SET OF DATA
-      plot(x.+num_x, data, color="red", linewidth=2.0, linestyle="--")
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      plot(data, color="green", linewidth=2.0)
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      println("DAA=$key1")
-elseif (key1 == 100)# D buttom
-      PyPlot.cla()
-      println("DAA=$key1")
-
-      num_x = num_x + 10
-      xc = x .+ num_x
-      data2 = j_f  # SECOND SET OF DATA
-      plot(x.-num_x, data, color="red", linewidth=2.0, linestyle="--")
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      plot(data, color="green", linewidth=2.0)
-                  title("AUGER")
-                  xlabel("X")
-                  ylabel("Y")
-                  grid("on")
-      println("DAA=$key1")
+elseif (key1 == 100)################### D buttom
+        num_x = num_x + 10
+        Plot_me(x, data, (x .- num_x), data)
 
 end
 
